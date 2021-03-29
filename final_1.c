e82bd8d52b4dc43c400ca829cc1fa8fd47279f52
SaThitiphon <78350405+SaThitiphon@users.noreply.github.com>
2021-03-29 17:26:51 +0700

    Update final_1.c

--- final_1.c
+++ final_1.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 int main()
 {
-	int n,c,k,m;
+	int n,c,k,m,o;
 	scanf("%d %d",&n,&c);
 	for(k=n;k<=100*n;k++)
 	{
 		m = k % 10;
+		o = n / 10;
+		if(n%10!=9)
+		{
+			if(o==c)
+			{
+				k++;
+				break;
+			}
+		}
 		if(m==c&&k>n)
 		{
-			printf("%d",k);
 			break;
 		}
 	}
+	printf("%d",k);
 }

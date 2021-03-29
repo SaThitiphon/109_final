52403b2b02c4536a8067afa126af3ffcaca53dc4
SaThitiphon <78350405+SaThitiphon@users.noreply.github.com>
2021-03-29 18:19:12 +0700

    แก้ใหม่หมด

--- final_2.c
+++ final_2.c
@@ -1,33 +1,35 @@
 #include <stdio.h>
 int main()
 {
-	int n,j,i;
+	int n,j,i,k;
 	scanf("%d",&n);
-	int a[n][2];
+	int a[n],c1[10000];
+	for(i=0;i<10000;i++)
+	{
+		c1[i] = 0;
+	}
 	for(i=0;i<n;i++)
 	{
 		scanf("%d",&a[i]);
 	}
-	for(i=0;i<n;i++)
+	for(i=1;i<=10000;i++)
 	{
 		for(j=0;j<n;j++)
 		{
-			if(a[i]==a[j])
+			if(i==a[j])
 			{
-				a[i][1]++;
+				c1[i-1]++;
 			}
 		}
 	}
-	for(i=0;i<n;i++)
+	int max;
+	max = c1[0];
+	for(i=0;i<10000;i++)
 	{
-		for(j=0;j<2;j++)
+		if(max<c1[i])
 		{
-			if(a[i]==a[j])
-			{
-				printf("%d ",a[i][j])
-			}
-			printf("\n");
+			max = c1[i];
 		}
 	}
-	
+	printf("%d",max);
 }
